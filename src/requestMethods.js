d65c9f174b36b6819a131302a276c0e6db18eb86
TimBa1 <timilehinbakare.tb@gmail.com>
2022-06-29 20:46:24 +0100

    user

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYmNhYTE1NTdhYzBlNTQ5NmEwNTJmYiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTY1MzE1MTAsImV4cCI6MTY1Njc5MDcxMH0.hZ0sG7z54q6Z_wr_dt-JvH_leOg-_Tb31uPxQXIeMhQ"
+// JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
